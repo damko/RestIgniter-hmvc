e78ad96e84ed9e82d631af4a5d2f0ed138c51c62
Damiano Venturin <dam@venturin.net>
2011-10-15 00:13:35 +0200

    OPEN - task 2: tests refactoring http://github.com/damko/Contact-Engine/issues/issue/2
    added class Test_Controller for better usability

--- application/controllers/api_tests.php
+++ application/controllers/api_tests.php
@@ -2,31 +2,31 @@
 // Created on Oct 14, 2011 by Damiano Venturin @ Squadra Informatica
 // Php unit tests
 
-class Api_Tests extends CI_Controller {
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH.'/libraries/test_controller.php';
+
+class Api_Tests extends Test_Controller {
 
 	public function __construct()
-	{
+	{	
 		parent::__construct();
 		
-		//load the embedded unit_test library
-		$this->load->library('unit_test');
-		
-/* 		$this->unit->set_test_items(array('test_name', 'result'));
+		/*  
+	 	//set up a new template for the tests output 		
+		$this->unit->set_test_items(array('test_name', 'result'));
 		$str = '
 		<table border="0" cellpadding="4" cellspacing="1">
-		    {rows}
-		        <tr>
-		        <td>{item}</td>
-		        <td>{result}</td>
-		        </tr>
-		    {/rows}
+		{rows}
+		<tr>
+		<td>{item}</td>
+		<td>{result}</td>
+		</tr>
+		{/rows}
 		</table>';
 		
-		$this->unit->set_template($str); */
-		
-		//load the rest client
-		$this->load->spark('restclient/2.0.0');		
-		
+		$this->unit->set_template($str); 
+		*/
+				
 		//set the server page
 		$this->rest->initialize(array('server' => $this->config->item('rest_server').'/exposeObj/chartex/'));
 	}
@@ -71,42 +71,7 @@ class Api_Tests extends CI_Controller {
 		$this->check200($method, $rest_return);
 		
 		$this->printReturn($rest_return);
-	}	
-	
-	private function arrayReturn($method, $rest_return, $note = null)
-	{
-		echo $this->unit->run($rest_return, 'is_array', $method.'- array in return ?', $note);
-	}
-	
-	private function anyError($rest_return)
-	{
-		$test = (array) $rest_return['status'];		
-		return isset($test['error_message']) ? true : false;
-	}
-		
-	private function checkNoRestError($method, $rest_return, $note = null)
-	{
-		$expected_result = false;
-		$test = $this->anyError($rest_return);
-		echo $this->unit->run($test, $expected_result, $method.'- any REST error ?', $null);
-	}
-	
-	private function check200($method, $rest_return, $note = null)
-	{
-		$expected_result = '200';
-		$test = (array) $rest_return['status'];
-		$test = $test['status_code'];
-		echo $this->unit->run($test, $expected_result, $method.'- status code == 200 ?', $note);
-	}
-	
-	private function printReturn($rest_return)
-	{
-		echo '<h3>REST return</h3>';
-		echo '<pre style="font-size: 9px; background-color: #e8e8e8;">';
-		print_r($rest_return);
-		echo '</pre>';
-	}	
-	
+	}		
 }
 
 /* End of api_tests.php */
